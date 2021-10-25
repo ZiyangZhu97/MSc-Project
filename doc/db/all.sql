1ee57ae01e41e01f43dc707bb80746183e2aa10b
Ziyang Zhu <2599143z@student.gla.ac.uk>
2021-10-26 00:05:30 +0100

    1. better mybatis logs 2. use devtools 3. database script

---
+++ doc/db/all.sql
@@ -0,0 +1,5 @@
+
+insert into `test` (id, name) values (1, 'test');
+insert into `test` (id, name) values (2, 'test2');
+insert into `test` (id, name) values (4, 'test4');
+insert into test (id, name) values (3, 'test3');
