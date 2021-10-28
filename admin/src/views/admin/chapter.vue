afe9491fc877b9d99990ca0b6960d388dbfe2631
Ziyang Zhu <2599143z@student.gla.ac.uk>
2021-10-28 02:00:54 +0100

    1. use gateway to redirect port

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -90,8 +90,8 @@
     methods: {
       list() {
         let _this = this;
-        _this.$ajax.get('http://127.0.0.1:9002/business/admin/chapter/list').then((response)=>{
-          console.log("查询大章列表结果：", response);
+        _this.$ajax.get('http://127.0.0.1:9000/business/admin/chapter/list').then((response)=>{
+          console.log("查询章节列表结果：", response);
           _this.chapters = response.data;
         })
       }
