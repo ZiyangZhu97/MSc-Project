8bb0cd806d8102a3e4c12e965c8b046d298f2dba
Runze <2556039l@student.gla.ac.uk>
2021-10-29 18:31:18 +0100

    1. EnumGenerator

--- admin/public/static/js/enums.js
+++ admin/public/static/js/enums.js
@@ -1,1 +1,5 @@
-CHARGE = [{key:"C", value:"Charge"},{key:"F", value:"Free"}];
+EPISODE_CHARGE={CHARGE:{key:"C", value:"Charge"},FREE:{key:"F", value:"Free"}};
+YES_NO={YES:{key:"1", value:"Yes"},NO:{key:"0", value:"No"}};
+
+EPISODE_CHARGE_ARRAY=[{key:"C", value:"Charge"},{key:"F", value:"Free"}];
+YES_NO_ARRAY=[{key:"1", value:"Yes"},{key:"0", value:"No"}];
