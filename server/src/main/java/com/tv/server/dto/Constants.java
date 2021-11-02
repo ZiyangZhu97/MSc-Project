9f703df12a0cd23e14e986afba81f8b2cc64510a
Ziyang Zhu <2556039l@student.gla.ac.uk>
2021-11-02 18:59:26 +0000

    1. logout

---
+++ server/src/main/java/com/tv/server/dto/Constants.java
@@ -0,0 +1,6 @@
+package com.tv.server.dto;
+
+public class Constants {
+
+    public static final String LOGIN_USER = "LOGIN_USER";
+}
--- system/src/main/java/com/tv/system/controller/admin/UserController.java
+++ system/src/main/java/com/tv/system/controller/admin/UserController.java
@@ -1,9 +1,6 @@
 package com.tv.system.controller.admin;
 
-import com.tv.server.dto.LoginUserDto;
-import com.tv.server.dto.UserDto;
-import com.tv.server.dto.PageDto;
-import com.tv.server.dto.ResponseDto;
+import com.tv.server.dto.*;
 import com.tv.server.service.UserService;
 import com.tv.server.util.ValidatorUtil;
 import org.slf4j.Logger;
@@ -12,6 +9,7 @@ import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/admin/user")
@@ -77,11 +75,24 @@ public class UserController {
      * 登陆
      */
     @PostMapping("/login")
-    public ResponseDto login(@RequestBody UserDto userDto) {
+    public ResponseDto login(@RequestBody UserDto userDto, HttpServletRequest request) {
         userDto.setPassword(DigestUtils.md5DigestAsHex(userDto.getPassword().getBytes()));
         ResponseDto responseDto = new ResponseDto();
         LoginUserDto loginUserDto = userService.login(userDto);
+
+        request.getSession().setAttribute(Constants.LOGIN_USER, loginUserDto);
+
         responseDto.setContent(loginUserDto);
+        return responseDto;
+    }
+
+    /**
+     * 退出登录
+     */
+    @GetMapping("/logout")
+    public ResponseDto logout(HttpServletRequest request) {
+        ResponseDto responseDto = new ResponseDto();
+        request.getSession().removeAttribute(Constants.LOGIN_USER);
         return responseDto;
     }
 }
