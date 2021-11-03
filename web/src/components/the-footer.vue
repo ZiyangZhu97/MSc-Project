bec3a36d95337898df940dc90e010426ab9259a4
Ziyang Zhu <2556039l@student.gla.ac.uk>
2021-11-03 21:10:49 +0000

    1. change text

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -4,8 +4,7 @@
       <p class="float-right">
         <a href="#">Back to top</a>
       </p>
-      <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
-      <p>New to Bootstrap? <a href="https://v4.bootcss.com/">Visit the homepage</a> or read our <a href="/docs/getting-started/introduction/">getting started guide</a>.</p>
+      <p>Welcome to ZZY Videos!</p>
     </div>
   </footer>
 </template>
