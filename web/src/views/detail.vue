e043438612ba86dac5c45d4acb5b82322d9a6a22
Ziyang Zhu <2556039l@student.gla.ac.uk>
2021-11-04 20:07:02 +0000

    1. show seasons and episodes

--- web/src/views/detail.vue
+++ web/src/views/detail.vue
@@ -18,9 +18,54 @@
               <span class="price-now text-danger"><i class="fa fa-gbp"></i>&nbsp;{{program.price}}&nbsp;&nbsp;</span>
             </p>
             <p class="program-head-button-links">
-              <a class="btn btn-lg btn-primary btn-shadow" href="javascript:;">like</a>
+              <a class="btn btn-lg btn-primary btn-shadow" href="javascript:;">Subscribe</a>
             </p>
           </div>
+        </div>
+
+        <div class="row">
+
+          <!-- season episode -->
+          <div class="col-md-9">
+            <!-- Nav tabs -->
+            <ul class="nav nav-tabs">
+              <li class="nav-item">
+                <a class="nav-link" href="#chapter" data-toggle="tab">Contents</a>
+              </li>
+            </ul>
+
+            <br>
+
+            <!-- Tab panes -->
+            <div class="tab-content">
+              <div class="tab-pane active" id="chapter">
+                <div v-for="chapter in chapters" class="chapter">
+                  <div class="chapter-chapter">
+                    <span class="folded-button">{{chapter.name}}</span>
+                  </div>
+                  <div>
+                    <table class="table table-striped">
+                      <tr v-for="(s, j) in chapter.episodes" class="chapter-episode-tr">
+                        <td class="col-sm-8 col-xs-12">
+                          <div class="episode-title">
+                            <i class="fa fa-video-camera d-none d-sm-inline"></i>&nbsp;&nbsp;
+                            <span class="d-none d-sm-inline">Episode {{j+1}}&nbsp;&nbsp;</span>
+                            {{s.title}}
+                            <span v-show="s.charge !== EPISODE_CHARGE.CHARGE.key" class="badge badge-primary hidden-xs">free</span>
+                          </div>
+                        </td>
+                        <td class="col-sm-1 text-right">
+                          <span class="badge badge-primary">{{s.time | formatSecond}} s</span>
+                        </td>
+                      </tr>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
         </div>
       </div>
     </div>
@@ -38,7 +83,8 @@
         program: {},
         chapters: [],
         episodes: [],
-        PROGRAM_TYPE: PROGRAM_TYPE
+        PROGRAM_TYPE: PROGRAM_TYPE,
+        EPISODE_CHARGE: EPISODE_CHARGE
       }
     },
     mounted() {
@@ -54,6 +100,20 @@
           _this.program = resp.content;
           _this.chapters = _this.program.chapters || [];
           _this.episodes = _this.program.episodes || [];
+
+          // 将所有的节放入对应的章中
+          Tool.sortAsc(_this.chapters, "id");
+          for (let i = 0; i < _this.chapters.length; i++) {
+            let c = _this.chapters[i];
+            c.episodes = [];
+            for (let j = 0; j < _this.episodes.length; j++) {
+              let s = _this.episodes[j];
+              if (c.id === s.chapterId) {
+                c.episodes.push(s);
+              }
+            }
+            Tool.sortAsc(c.episodes, "sort");
+          }
         })
       },
     }
@@ -87,4 +147,36 @@
       font-size: 1.5rem;
     }
   }
+
+    /* 章节列表 */
+  .chapter {
+    padding-bottom: 1.25rem;
+  }
+  .chapter-chapter {
+    font-size: 1.25rem;
+    padding: 1.25rem;
+    background-color: #23527c;
+    color: white;
+  }
+  .chapter-episode-tr {
+    font-size: 1rem;
+  }
+  .chapter-episode-tr td{
+    padding: 1rem 1.25rem;
+    vertical-align: middle;
+  }
+  /*鼠标手势*/
+  .chapter-episode-tr td .episode-title{
+    color: #555;
+  }
+  .chapter-episode-tr td .episode-title:hover{
+    color: #23527c;
+    font-weight: bolder;
+    cursor: pointer;
+  }
+  /*行头小图标*/
+  .chapter-episode-tr td .episode-title i{
+    color: #2a6496;
+  }
+
 </style>
